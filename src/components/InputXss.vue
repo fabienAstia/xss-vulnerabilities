12e6009a05d60990ce3a0b33ba9c2e41e47a1f97
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2024-10-03 21:58:26 +0200

    initializing some interface front -> very slow reactivitygit add .!

--- src/components/InputXss.vue
+++ src/components/InputXss.vue
@@ -1,8 +1,35 @@
 <script>
-
+    import {ref} from 'vue'
+    const bypass = ref('')
 </script>
 
 <template>
+    <h1>How to illustrate XSS vulnerabilities</h1>
+    <div class="m-3">
+        <h4>Window</h4>
+        <div class="window p-3">
+                {{ bypass}}
+        </div>
+    </div>
+
+    <div class="m-3">
+        <label for="">Try a bypass: </label>
+        <input type="text"  class="form-control" v-model="bypass">
+        <button class="btn btn-primary mt-3">submit </button>
+    </div>
+   
+    <div class="m-3">
+        <textarea name="" id="" class="form-control"></textarea>
+        <button class="btn btn-primary mt-3">submit </button>
+    </div>
     
+   
 </template>
+
+<style scoped>
+    .window{
+        height: 200px;
+        border: 2px solid black;
+    }
+</style>
 
